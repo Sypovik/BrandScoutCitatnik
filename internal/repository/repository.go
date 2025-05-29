c239b34ff4bd33f06b15771f9e391675074f2d81
sypovik <nekich0999@gmail.com>
2025-05-30 02:09:21 +0700

    Добавлена обстракция

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,14 @@
 package repository
+
+import "BrandScoutCitatnik/internal/models"
+
+type QuoteRepository interface {
+	Add(quote models.Quote) (models.Quote, error)
+
+	GetAll() ([]models.Quote, error)
+	GetRandom() (*models.Quote, error)
+
+	GetByAuthor(author string) ([]models.Quote, error)
+
+	Delete(id int) error
+}
