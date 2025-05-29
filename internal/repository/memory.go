0ca41606a50504bd7e61b58e311af19b7c4b0023
sypovik <nekich0999@gmail.com>
2025-05-29 20:48:19 +0700

    Добавить реализацию MemoryQuoteRepository и тесты для методов добавления и получения цитат

--- internal/repository/memory.go
+++ internal/repository/memory.go
@@ -1,1 +1,70 @@
 package repository
+
+import (
+	"BrandScoutCitatnik/internal/models"
+	"errors"
+	"math/rand"
+	"sync"
+)
+
+type MemoryQuoteRepository struct {
+	sync.RWMutex
+	data   map[int]models.Quote
+	nextID int
+}
+
+func NewMemoryQuoteRepository() *MemoryQuoteRepository {
+	return &MemoryQuoteRepository{
+		data:   make(map[int]models.Quote),
+		nextID: 1,
+	}
+}
+
+func (m *MemoryQuoteRepository) Add(quote models.Quote) (models.Quote, error) {
+	m.Lock()
+	defer m.Unlock()
+
+	quote.ID = m.nextID
+	m.data[m.nextID] = quote
+	m.nextID++
+
+	return quote, nil
+}
+
+func (m *MemoryQuoteRepository) GetAll() ([]models.Quote, error) {
+	m.RLock()
+	defer m.RUnlock()
+	var arrQuotes []models.Quote
+	// arrQuotes := make([]models.Quote, 0, len(m.data))
+
+	for _, i := range m.data {
+		arrQuotes = append(arrQuotes, i)
+	}
+	return arrQuotes, nil
+}
+
+// !!!Не работает потому, что при удалении, в данном подходе, ID может не быть
+// !!!Нужно смотреть какие ID есть и среди них выводить случайный
+// !!!Добавить зерно сида
+
+func (m *MemoryQuoteRepository) GetRandom() (*models.Quote, error) {
+	m.RLock()
+	defer m.RUnlock()
+
+	if len(m.data) == 0 {
+		return nil, errors.New("no quotes available")
+	}
+
+	randomID := rand.Intn(len(m.data))
+	quote := m.data[randomID]
+
+	return &quote, nil
+}
+
+// func (m *MemoryQuoteRepository) GetByAuthor(author string) ([]models.Quote, error) {
+
+// }
+
+// func (m *MemoryQuoteRepository) Delete(id int) error {
+
+// }
---
+++ internal/repository/memory_test.go
@@ -0,0 +1,95 @@
+package repository
+
+import (
+	"BrandScoutCitatnik/internal/models"
+	"testing"
+)
+
+func TestSaveAndFindAll(t *testing.T) {
+	repo := NewMemoryQuoteRepository()
+
+	q := models.Quote{
+		Author: "Confucius",
+		Quote:  "Life is simple, but we insist on making it complicated.",
+	}
+
+	_, err := repo.Add(q)
+	if err != nil {
+		t.Fatalf("unexpected error on Save: %v", err)
+	}
+
+	all, err := repo.GetAll()
+	if err != nil {
+		t.Fatalf("unexpected error on FindAll: %v", err)
+	}
+
+	if len(all) != 1 {
+		t.Errorf("expected 1 quote, got %d", len(all))
+	}
+
+	if all[0].ID != 1 {
+		t.Errorf("expected ID 1, got %d", all[0].ID)
+	}
+}
+
+// func TestFindByAuthor(t *testing.T) {
+// 	repo := NewMemoryQuoteRepository()
+
+// 	quotes := []models.Quote{
+// 		{Author: "Confucius", Quote: "One"},
+// 		{Author: "Aristotle", Quote: "Two"},
+// 		{Author: "Confucius", Quote: "Three"},
+// 	}
+
+// 	for _, q := range quotes {
+// 		_,_ = repo.Add(q)
+// 	}
+
+// 	found, err := repo.GetByAuthor("Confucius")
+// 	if err != nil {
+// 		t.Fatalf("unexpected error: %v", err)
+// 	}
+// 	if len(found) != 2 {
+// 		t.Errorf("expected 2 quotes, got %d", len(found))
+// 	}
+// }
+
+func TestFindRandom(t *testing.T) {
+	repo := NewMemoryQuoteRepository()
+
+	_, err := repo.GetRandom()
+	if err == nil {
+		t.Error("expected error on empty repository")
+	}
+
+	_, _ = repo.Add(models.Quote{Author: "A", Quote: "1"})
+	_, _ = repo.Add(models.Quote{Author: "B", Quote: "2"})
+	_, _ = repo.Add(models.Quote{Author: "C", Quote: "3"})
+	_, _ = repo.Add(models.Quote{Author: "D", Quote: "4"})
+	_, _ = repo.Add(models.Quote{Author: "D", Quote: "5"})
+
+	q, err := repo.GetRandom()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if q == nil {
+		t.Error("expected non-nil quote")
+	}
+}
+
+// func TestDelete(t *testing.T) {
+// 	repo := NewMemoryQuoteRepository()
+
+// 	q := models.Quote{Author: "A", Quote: "Q"}
+// 	_,_ = repo.Add(q)
+
+// 	err := repo.Delete(1)
+// 	if err != nil {
+// 		t.Fatalf("unexpected error: %v", err)
+// 	}
+
+// 	err = repo.Delete(1)
+// 	if err == nil {
+// 		t.Error("expected error when deleting non-existent ID")
+// 	}
+// }
